296d7994bec67d5d1b48c137667048fdc559df2d
Chris <4147107+bluesign2k@users.noreply.github.com>
2017-12-17 18:14:51 +0000

    Added Support For 7" CPLD
    Added support for 7" displays using CPLD drivers

--- src/Configuration.hpp
+++ src/Configuration.hpp
@@ -11,12 +11,13 @@
 
 #include <cstdint>
 
-#define VERSION_TEXT		"1.17"
+#define VERSION_TEXT		"1.17.1"
 
 #define DISPLAY_TYPE_ITDB02_43			(1)		// Itead 4.3 inch display (480 x 272) or alternative 4.3 inch display with 24-bit colour
 #define DISPLAY_TYPE_ITDB02_50			(2)		// Itead 5.0 inch display (800 x 480) or alternative 5 or 7 inch display with 24-bit colour
 #define DISPLAY_TYPE_ITDB02_70			(3)		// 7.0 inch display (800 x 480) with 18-bit colour
 #define DISPLAY_TYPE_ER_70				(4)		// 7.0 inch East Rising display (800 x 480) with 24-bit colour
+#define DISPLAY_TYPE_CPLD_70			(5)		// 7.0 inch CPLD display (800 x 480) with 24-bit colour
 
 // Define DISPLAY_TYPE to be one of the above 3 types of display
 
@@ -48,6 +49,12 @@
 #define IS_ER			(true)
 #endif
 
+#ifdef SCREEN_70CPLD
+#define DISPLAY_TYPE	DISPLAY_TYPE_CPLD_70
+#define LARGE_FONT		(1)
+#define IS_24BIT		(true)
+#define IS_ER			(false)
+#endif
 
 const uint32_t DefaultBaudRate = 57600;
 
