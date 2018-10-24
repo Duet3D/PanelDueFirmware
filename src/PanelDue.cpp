060c49f7c2e629741e382246eb9240ed2368fe2c
garyd9 <garyd9@hotmail.com>
2018-10-24 13:04:04 -0400

    When "off", a device is idle
    For the purpose of dimming a display when it is idle, consider the "off" state as being the same as idle.

--- src/PanelDue.cpp
+++ src/PanelDue.cpp
@@ -506,7 +506,7 @@ extern void RestoreBrightness()
 extern void DimBrightness()
 {
 	if (   (GetDisplayDimmerType() == DisplayDimmerType::always)
-		|| (GetDisplayDimmerType() == DisplayDimmerType::onIdle && status == PrinterStatus::idle)
+		|| (GetDisplayDimmerType() == DisplayDimmerType::onIdle && (status == PrinterStatus::idle) || (status == PrinterStatus::off))
 	   )
 	{
 		Buzzer::SetBacklight(nvData.brightness/8);
