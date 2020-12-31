e8c5255dbc13963f7a35282fe4a1a7100ebfcb2d
vitaliy172 <68148475+vitaliy172@users.noreply.github.com>
2020-12-31 10:25:58 +0200

    Update Strings.hpp

--- src/Strings.hpp
+++ src/Strings.hpp
@@ -20,7 +20,7 @@
 #define Newline      "\n"
 #define DegreeSymbol  "\u00B0"
 
-constexpr unsigned int NumLanguages = 5;
+constexpr unsigned int NumLanguages = 6;
 constexpr unsigned int NumStatusStrings = 14;
 
 struct StringTable
