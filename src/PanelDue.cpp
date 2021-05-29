8d8eadd4d27998eb186477ea8c8b21cbce18245b
Michael Szafranek <faule.aepfel@gmail.com>
2021-05-29 14:02:00 +0200

    PanelDue: added messagelog when a reset was detected

--- src/PanelDue.cpp
+++ src/PanelDue.cpp
@@ -1766,6 +1766,7 @@ static void ProcessReceivedValue(StringRef id, const char data[], const size_t i
 				if (uival < remoteUpTime)
 				{
 					Reconnect();
+					MessageLog::AppendMessageF("Printer reset detected");
 				}
 				remoteUpTime = uival;
 			}
