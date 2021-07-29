3631b52695d820e17e538e30b5113b55f0e328e4
Michael Szafranek <faule.aepfel@gmail.com>
2021-07-29 11:23:19 +0200

    Version: bumped version

--- src/Version.hpp
+++ src/Version.hpp
@@ -8,7 +8,7 @@
 #ifndef SRC_VERSION_HPP_
 #define SRC_VERSION_HPP_
 
-#define VERSION_TEXT_MAIN		"3.3.0-rc3"
+#define VERSION_TEXT_MAIN		"3.3.0-rc4"
 
 #ifdef SUPPORT_ENCODER
 #define VERSION_TEXT	VERSION_TEXT_MAIN "+enc"
