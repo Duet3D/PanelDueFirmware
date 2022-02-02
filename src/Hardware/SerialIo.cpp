4c14d0b2c747c3b79e61822a643775b256764501
Michael Szafranek <faule.aepfel@gmail.com>
2022-02-02 19:31:09 +0100

    SerialIo: increased filed buffer to provide space for terminating zero for buffers up to 1024 bytes

--- src/Hardware/SerialIo.cpp
+++ src/Hardware/SerialIo.cpp
@@ -231,7 +231,7 @@ namespace SerialIo
 
 	// fieldId is the name of the field being received. A '^' character indicates the position of an _ecv_array index, and a ':' character indicates a field separator.
 	String<100> fieldId;
-	String<1024> fieldVal;
+	String<1028> fieldVal;
 	size_t arrayIndices[MaxArrayNesting];
 	size_t arrayDepth = 0;
 
