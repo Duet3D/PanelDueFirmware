7111d74ce1ba8902770f3b72a84d55c6605970ea
Michael Szafranek <faule.aepfel@gmail.com>
2022-04-28 13:57:58 +0200

    Utils: removed unused code

--- src/ObjectModel/Utils.cpp
+++ src/ObjectModel/Utils.cpp
@@ -19,60 +19,8 @@ namespace OM
 			const size_t heaterIndex,
 			Slots& slots,
 			const SlotType slotType,
-			const bool addTools,
-			const bool addBeds,
-			const bool addChambers)
+			const bool addTools)
 	{
-		if (addBeds)
-		{
-			IterateBedsWhile(
-				[&slots, &heaterIndex, &slotType](Bed*& bed, size_t) {
-					if (bed->heater == (int)heaterIndex)
-					{
-						switch (slotType)
-						{
-						case SlotType::panel:
-							if (bed->slot < MaxSlots)
-							{
-								slots.Add(bed->slot);
-							}
-							break;
-						case SlotType::pJob:
-							if (bed->slotPJob < MaxPendantTools)
-							{
-								slots.Add(bed->slotPJob);
-							}
-							break;
-						}
-					}
-					return bed->slot < MaxSlots;
-				});
-		}
-		if (addChambers)
-		{
-			IterateChambersWhile(
-				[&slots, &heaterIndex, &slotType](Chamber*& chamber, size_t) {
-					if (chamber->heater == (int)heaterIndex)
-					{
-						switch (slotType)
-						{
-						case SlotType::panel:
-							if (chamber->slot < MaxSlots)
-							{
-								slots.Add(chamber->slot);
-							}
-								break;
-						case SlotType::pJob:
-							if (chamber->slotPJob < MaxPendantTools)
-							{
-								slots.Add(chamber->slotPJob);
-							}
-							break;
-						}
-					}
-					return chamber->slot < MaxSlots;
-				});
-		}
 		if (addTools)
 		{
 			IterateToolsWhile(
