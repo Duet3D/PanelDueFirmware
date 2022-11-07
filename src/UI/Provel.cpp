4d5b1e162c879e61a9a94d0c496d17eaf15157de
Michael Szafranek <faule.aepfel@gmail.com>
2022-11-07 13:29:17 +0100

    Provel: added initial files for provel to build system

---
+++ src/UI/Provel.cpp
@@ -0,0 +1,11 @@
+#include "UI/Provel.hpp"
+
+// TODO
+//
+// - user interface
+//   - 1 column
+//   - 6 rows
+//     - static/dynamic text fields
+//     - button
+//       - half width
+//       - full full
---
+++ src/UI/Provel.hpp
@@ -0,0 +1,4 @@
+#ifndef UI_PROVEL_HPP
+#define UI_PROVEL_HPP 1
+
+#endif
