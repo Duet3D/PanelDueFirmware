d27f3a8e6e3ff5c3f2c148d65bce0aa637345171
Michael Szafranek <faule.aepfel@gmail.com>
2022-11-08 17:22:35 +0100

    Version: set custom version

--- src/Version.hpp
+++ src/Version.hpp
@@ -10,10 +10,6 @@
 
 #define VERSION_TEXT_MAIN		"3.4.2-pre1"
 
-#ifdef SUPPORT_ENCODER
 #define VERSION_TEXT	VERSION_TEXT_MAIN "+provel"
-#else
-#define VERSION_TEXT	VERSION_TEXT_MAIN
-#endif
 
 #endif /* SRC_VERSION_HPP_ */
