425874ddc76f13f1a23ac2f2892d4ffcbbea9164
Michael Szafranek <faule.aepfel@gmail.com>
2022-11-08 17:16:43 +0100

    Provel: added draft for new user interface concept

--- src/UI/Provel.cpp
+++ src/UI/Provel.cpp
@@ -5,8 +5,18 @@
  *      Author: Michael Szafranek (faule.aepfel@gmail.com)
  */
 
-// TODO
-// user interface
+#include "UI/Provel.hpp"
+
+#ifdef assert
+#undef assert
+#endif
+#include <cassert>
+
+#include "Version.hpp"
+
+#define DEBUG 2
+#include "Debug.hpp"
+
 //
 // - generic screen
 //   - 1 column
@@ -15,6 +25,12 @@
 //     - button
 //       - half width
 //       - full full
+//
+// - testing
+//   - good example screens
+//     - splash
+//     - homing
+//     - print status
 //
 // - screens
 //   - splash
@@ -30,39 +46,195 @@
 //   - fault
 //   - warning
 
-#include "UI/Provel.hpp"
+// questions
+//
+// - static or dynamic screen allocation?
+// - how to state information?
+//   - a part is already stored in global variables
+//   - a part is stored in object model structures
+// - how is state stored?
+//   - probably makes sense to have ui state and a printer state
+//   - due to action triggered from the ui an itermediate state before transitioning from
+//     one ui state to another might be needed
+// - how are action transmitted to the printer?
+//
 
-#include "Configuration.hpp"
-#include "FileManager.hpp"
-#include "FlashData.hpp"
+namespace Provel {
 
-#include "General/SafeVsnprintf.h"
-#include "General/SimpleMath.h"
-#include "General/String.h"
-#include "General/StringFunctions.h"
+Provel::Provel() : screens(nullptr)
+{
+	colours = &colourSchemes[0];
 
-#include "Hardware/Buzzer.hpp"
-#include "Hardware/Reset.hpp"
-#include "Hardware/SerialIo.hpp"
-#include "Hardware/SysTick.hpp"
+	DisplayField::SetDefaultFont(DEFAULT_FONT);
+	DisplayField::SetDefaultColours(colours->infoTextColour, colours->infoBackColour);
+	SingleButton::SetTextMargin(textButtonMargin);
+	SingleButton::SetIconMargin(iconButtonMargin);
+}
 
-#include "Icons/Icons.hpp"
-#include "Library/Misc.hpp"
+Provel::~Provel()
+{
+}
 
-#include "ObjectModel/Axis.hpp"
-#include "ObjectModel/PrinterStatus.hpp"
-#include "ObjectModel/Utils.hpp"
+Screen *Provel::Push(Screen *screen)
+{
+	int ret;
 
-#include "UI/Display.hpp"
-#include "UI/MessageLog.hpp"
-#include "UI/Popup.hpp"
-#include "UI/Strings.hpp"
-#include "UI/UserInterface.hpp"
-#include "UI/UserInterfaceConstants.hpp"
-#include "Version.hpp"
+	dbg("\r\n");
 
-#define DEBUG 0
-#include "Debug.hpp"
+	assert(screen);
 
-static MainWindow *Provel;
+	ret = screen->Init(colours);
+	assert(ret == 0);
 
+	screen->next = screens;
+	screens = screen;
+
+	return screens;
+}
+
+Screen *Provel::Pop()
+{
+	Screen *head;
+
+	dbg("\r\n");
+
+	assert(screens);
+
+	head = screens;
+	screens = screens->next;
+
+	head->Shutdown();
+
+	return head;
+}
+
+int Provel::Update()
+{
+	Screen *head = screens;
+
+	dbg("\r\n");
+
+	if (!head)
+		return 0;
+
+	return head->Update();
+}
+
+int Provel::FindTouch(int x, int y)
+{
+	dbg("\r\n");
+
+	return 0;
+}
+
+int Provel::ProcessTouch(ButtonPress &event)
+{
+	dbg("\r\n");
+
+	return 0;
+}
+
+Screen *Provel::Reset(Screen *screen)
+{
+	dbg("\r\n");
+
+	for (Screen *head = screens; head; head = head->next) {
+		int ret = head->Shutdown();
+		assert(ret == 0);
+	}
+
+	return Push(screen);
+}
+
+
+int Screen::Init(const ColourScheme *colours)
+{
+	assert(colours);
+
+	dbg("\r\n");
+
+	root.Init(colours->defaultBackColour);
+	//root.Init(UTFT::fromRGB(128, 0, 0));
+	dbg("\r\n");
+	root.Refresh(true);
+
+	return 0;
+}
+
+int Screen::Update()
+{
+	dbg("\r\n");
+
+	root.Refresh(false);
+
+	return 0;
+};
+
+int Screen::Shutdown()
+{
+	return 0;
+};
+
+int Screen::Add(Element *element)
+{
+	dbg("\r\n");
+
+	assert(element);
+
+	root.AddField(element->Get());
+
+	return 0;
+}
+
+int Screen::Delete(Element *element)
+{
+	assert(element);
+
+	return 0;
+}
+
+
+Text::Text(PixelNumber x, PixelNumber y, PixelNumber width, PixelNumber height, const char *ptext) :
+	text(y, x, width, TextAlignment::Left, ptext, false)
+{
+}
+
+Title::Title(PixelNumber x, PixelNumber y, PixelNumber width, PixelNumber height, const char *title) :
+	text(y, x, width, TextAlignment::Left, title, false)
+{
+}
+
+#define PROVEL_WIDTH 500
+
+ScreenSplash::ScreenSplash()
+{
+	dbg("\r\n");
+	timeout = 1000;
+	title = new Title(100, 0, PROVEL_WIDTH, 100, "title: PROVEL");
+	version = new Text(100, 100, PROVEL_WIDTH, 100, "text: " VERSION_TEXT);
+
+	Add(title);
+	Add(version);
+}
+
+ScreenSplash::~ScreenSplash()
+{
+}
+
+
+ScreenHoming::ScreenHoming()
+{
+	title = new Title(100, 0, PROVEL_WIDTH, 100, "title: HOMING");
+	stop = new Button(100, 100, PROVEL_WIDTH, 100, "button: STOP", 0, 0);
+	status = new Status(100, 200, PROVEL_WIDTH, 100);
+
+	Add(title);
+	Add(stop);
+	Add(status);
+}
+
+ScreenHoming::~ScreenHoming()
+{
+}
+
+}
--- src/UI/Provel.hpp
+++ src/UI/Provel.hpp
@@ -1,4 +1,175 @@
 #ifndef UI_PROVEL_HPP
 #define UI_PROVEL_HPP 1
 
+#include "UI/ColourSchemes.hpp"
+#include "UI/Display.hpp"
+#include "UI/UserInterfaceConstants.hpp"
+
+#include "Version.hpp"
+
+namespace Provel {
+
+class Screen;
+class Element;
+
+// generic classes
+
+class Provel {
+
+private:
+	Screen *screens;
+
+	const ColourScheme *colours;
+
+public:
+	Provel();
+	~Provel();
+
+	// window stack functions
+	Screen *Push(Screen *screen);
+	Screen *Pop();
+	Screen *Reset(Screen *screen);
+
+	// event functions
+	int Update();
+	int FindTouch(int x, int y);
+	int ProcessTouch(ButtonPress &event);
+};
+
+class Screen {
+	MainWindow root;
+	Element *elements;
+
+public:
+	Screen *next;
+	Screen *prev;
+
+	// add and delete ui elements from screen
+	int Add(Element *element);
+	int Delete(Element *element);
+
+	// init or shutdown state machine
+	virtual int Init(const ColourScheme *colours);
+	virtual int Update();
+	virtual int Shutdown();
+
+	Screen() {
+		next = nullptr;
+		prev = nullptr;
+		elements = nullptr;
+	};
+	~Screen() {};
+};
+
+class Element {
+	Element *next;
+
+public:
+	virtual int Init() { return 0; };
+	virtual int Shutdown() { return 0; };
+
+	virtual int Update() { return 0; };
+
+	virtual DisplayField *Get() { return nullptr; };
+
+	Element() {
+		next = nullptr;
+	};
+	~Element();
+};
+
+
+class Button : public Element {
+	TextButton button;
+
+public:
+	Button(PixelNumber x, PixelNumber y, PixelNumber width, PixelNumber height, const char *text, event_t e, int param) :
+		button(x, y, width, text, e, param) {};
+	~Button() {};
+
+	DisplayField *Get() { return &button; };
+};
+
+class ButtonDouble : public Element {
+	TextButton button_left;
+	TextButton button_right;
+};
+
+class Text : public Element {
+	StaticTextField text;
+
+public:
+	Text(
+		PixelNumber x=0, PixelNumber y=0,
+		PixelNumber width=100, PixelNumber height=100,
+		const char *ptext="text");
+	~Text() {};
+
+	DisplayField *Get() { return &text; };
+};
+
+class Title : public Element {
+	StaticTextField text;
+
+public:
+	Title(
+		PixelNumber x=0, PixelNumber y=0,
+		PixelNumber width=100, PixelNumber height=100,
+		const char *title="title");
+	~Title();
+
+	DisplayField *Get() { return &text; };
+};
+
+class Status : public Element {
+	ColourGradientField status;
+
+	enum State {
+		Unknown,
+		Normal,
+		Warning,
+		Error
+	} state;
+
+	// will change dynamically depending on some state
+public:
+	Status(PixelNumber px, PixelNumber py, PixelNumber pw, PixelNumber ph) :
+		status(py, px, pw, ph) {
+			status.SetColours(UTFT::fromRGB(255,0,0), UTFT::fromRGB(0,128,0));
+		};
+	~Status() {};
+
+	void SetState(enum State val) { state = val; };
+	enum State GetState() { return state; };
+
+	DisplayField *Get() { return &status; }
+
+	virtual int Init() { return 0; };
+	virtual int Shutdown() { return 0; };
+
+	virtual int Update() { return 0; };
+};
+
+
+class ScreenSplash : public Screen {
+	Title *title;
+	Text *version;
+	int timeout; // in ms
+
+public:
+	ScreenSplash();
+	~ScreenSplash();
+};
+
+class ScreenHoming : public Screen {
+	Title *title;
+	Button *stop;
+	Status *status;
+
+public:
+	ScreenHoming();
+	~ScreenHoming();
+};
+}
+
 #endif
