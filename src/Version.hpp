b979ca36dbc3993820381f082b48d6513bf6f97a
Michael Szafranek <faule.aepfel@gmail.com>
2023-04-05 16:00:00 +0200

    Version: added device name to version string

--- src/Version.hpp
+++ src/Version.hpp
@@ -8,12 +8,22 @@
 #ifndef SRC_VERSION_HPP_
 #define SRC_VERSION_HPP_
 
-#define VERSION_TEXT_MAIN		"3.4.1"
+#define VERSION_MAIN		"3.4.1"
 
 #ifdef SUPPORT_ENCODER
-#define VERSION_TEXT	VERSION_TEXT_MAIN "+enc"
+#define VERSION_ENCODER	"+enc"
 #else
-#define VERSION_TEXT	VERSION_TEXT_MAIN
+#define VERSION_ENCODER
 #endif
+
+#ifdef DEVICE
+#define STRINGIFY(x)	#x
+#define TOSTRING(x)	STRINGIFY(x)
+#define VERSION_DEVICE "-" TOSTRING(DEVICE)
+#else
+#define VERSION_DEVICE
+#endif
+
+#define VERSION_TEXT VERSION_MAIN VERSION_ENCODER VERSION_DEVICE
 
 #endif /* SRC_VERSION_HPP_ */
